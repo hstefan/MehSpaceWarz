7545521ac17c2c73d418da56b2efc784ebb355e4
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-05-04 23:30:09 -0300

    Better update/rendering management.

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -15,24 +15,9 @@
 #include <ctime>
 #include <cstdlib>
 
-#ifdef __unix__
-#include <sys/time.h>
-#endif
-
-#ifdef __unix__
-struct timespec last_update;
-#else
-std::clock_t last_update = 0;
-#endif
 
 int main(int argc, char* argv[])
 {
-#ifdef __unix__
-   clock_gettime(CLOCK_REALTIME, &last_update);
-#else
-   last_update = std::clock();
-#endif
-
    glfwInit();
    glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
    glfwOpenWindow(800, 450, 8, 8, 8, 8, 24, 8, GLFW_WINDOW);
@@ -43,36 +28,33 @@ int main(int argc, char* argv[])
 
    bool running = true;
 
+   double last_render = 0;
+   double last_update = 0;
+
+   double frame_interval = 1.f/60.f;
+   double update_interval = 1.f/90.f;
+
+   double cur_time = 0;
+
    while(running) 
    {
+      cur_time = glfwGetTime();
       if(glfwGetKey(GLFW_KEY_ESC) == GLFW_PRESS) 
          break;
-
-#ifdef __unix__
-      static const double UPDATE_INTERVAL = 1. / 30.;
-      static const time_t UPDATE_INTERVAL_SEC = UPDATE_INTERVAL;
-      static const unsigned long UPDATE_INTERVAL_NS = (UPDATE_INTERVAL-UPDATE_INTERVAL_SEC) * 1E9;
-
-      struct timespec update_time;
-      clock_gettime(CLOCK_REALTIME, &update_time);
-
-      if (update_time.tv_sec - last_update.tv_sec >= UPDATE_INTERVAL_SEC &&
-         (unsigned long)update_time.tv_nsec - (unsigned long)last_update.tv_nsec >= UPDATE_INTERVAL_NS)
+      if(cur_time - last_render > frame_interval) 
+      {
+         last_render = glfwGetTime();
+         world->render();
+      }
+      
+     
+      if(cur_time - last_update > update_interval)
       {
          world->update();
-         clock_gettime(CLOCK_REALTIME, &last_update);
+         last_update = glfwGetTime();
       }
-#else
-      static const std::clock_t UPDATE_INTERVAL = CLOCKS_PER_SEC / 30;
-      world->update();
 
-      if (std::clock() >= last_update + UPDATE_INTERVAL)
-      {
-         world->render();
-         last_update += UPDATE_INTERVAL;
-      }
-#endif
-
+      glfwSleep(update_interval - (cur_time + glfwGetTime()));
       glfwSwapBuffers();
       running = glfwGetWindowParam(GLFW_OPENED) != 0;
    }
